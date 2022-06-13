572a424f8c96e6677aa0cfc4a9b03043b0047e75
Beatriz Lujan <bibilujan@gmail.com>
2022-06-13 11:27:45 -0400

    update regression tests

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -6,5 +6,8 @@ set -o pipefail
 #enter the workflow's final output directory ($1)
 cd $1
 
+# list output files to detect new or missing files
+ls -1
+
 #check md5sum for metrics json file
 cat *data.json | md5sum
